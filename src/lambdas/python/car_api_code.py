22a7ad16a030464008175f8746079e4f127f803a
cmedrano <carlosvalon75@gmail.com>
2023-05-06 00:39:10 +0200

    Basic functionalities of CarAPI + added test case

--- src/lambdas/python/car_api_code.py
+++ src/lambdas/python/car_api_code.py
@@ -10,20 +10,32 @@ def lambda_handler(event, context):
     body = json.loads(body)
     
     tabla_estado = dynamodb.Table('Estado')
+    tabla_electrolinera = dynamodb.Table('Electrolinera')
 
-    if (body['ID_PuntoCarga']): # Si la petición recibida tiene un ID_PuntoCarga, se devuelve el estado de ese punto de carga
+    if ('ID_PuntoCarga' in body): # Si la petición recibida tiene un ID_PuntoCarga, se devuelve el estado de ese punto de carga
         response = tabla_estado.get_item(Key={'ID_Estado': body['ID_PuntoCarga']}, TableName='Estado')
+        return {
+            'statusCode': 200,
+            'body': response['Item']
+        }
+    elif (('lat1' in body) and ('lat2' in body) and ('lon1' in body) and ('lon2' in body)): # Hemos recibido una petición de electrolineras en un área
+        # Hacemos una query a la tabla de electrolineras para obtener las electrolineras en el área
+        response = tabla_electrolinera.scan(
+            FilterExpression='ubic_lat BETWEEN :lat1 AND :lat2 AND ubic_lon BETWEEN :lon1 AND :lon2',
+            ExpressionAttributeValues={
+                ':lat1': body['lat1'],
+                ':lat2': body['lat2'],
+                ':lon1': body['lon1'],
+                ':lon2': body['lon2']
+            }
+        )
+        return {
+            'statusCode': 200,
+            'body': response#['Item']
+        }
+    else: # Si no, devolvemos un error
+        return {
+            'statusCode': 400,
+            'body': 'Bad request. No se ha especificado un ID_PuntoCarga o un área de búsqueda (campos lat1, lat2, lon1 y lon2).'
+        }
 
-    """if (body['ID_PuntoCarga']): # Si la petición recibida tiene un ID_PuntoCarga, se devuelve el estado de ese punto de carga
-        response = tabla_estado.query(KeyConditionExpression=Key('ID_Estado').eq(body['ID_PuntoCarga'])
-        #TableName='Estado',
-        #Key={
-        #    'ID_Estado': body['ID_PuntoCarga']
-        #}
-    )
-    """
-
-    return {
-        'statusCode': 200,
-        'body': response['Item']
-    }

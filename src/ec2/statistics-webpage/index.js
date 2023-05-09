5f20f96b7f53a29a5ffa16e05500c340321a895f
Jesús Jover <jesus.jover@alu.uclm.es>
2023-05-09 18:49:33 +0200

    Montaje del servidor web automático

--- src/ec2/statistics-webpage/index.js
+++ src/ec2/statistics-webpage/index.js
@@ -1,7 +1,7 @@
 // Import express
 const express = require('express')
 
-const port = 3000
+const port = 80
 
 //Set express to use body-parser as middle-ware.
 const app = express()
--- src/ec2/user_data.sh
+++ src/ec2/user_data.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-sudo -u ec2-user -i <<'EOF'
-
+sudo yum update && sudo yum install git -y && sudo -u ec2-user -i <<'EOF'
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 16
-
 mkdir abp
 cd abp
-
-git clone https://github.com/carlosmn25/ABP-PyGITIC.git
-cd ABP-PyGITIC
-git checkout origin/carlos
-git switch carlos
-
-cd src/ec2/statistics-webpage 
-npm install
-
-node index.js
+git clone https://github.com/carlosmn25/ABP-PyGITIC.git && cd ABP-PyGITIC/ && git switch -c origin/jesus &&
+cd src/ec2/statistics-webpage && npm i && npm -g install pm2 && 
+pm2 start index.js 
+pm2 startup 
+sudo env PATH=$PATH:/home/ec2-user/.nvm/versions/node/v16.20.0/bin /home/ec2-user/.nvm/versions/node/v16.20.0/lib/node_modules/pm2/bin/pm2 startup systemd -u ec2-user --hp /home/ec2-user 
+pm2 save
 EOF

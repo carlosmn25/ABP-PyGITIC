0780eb3e22b21499ad2a0e15cee7f48b91c7b611
Jesús Jover <jesus.jover@alu.uclm.es>
2023-05-09 13:33:11 +0200

    Updated user_data

--- src/ec2/user_data.sh
+++ src/ec2/user_data.sh
@@ -5,4 +5,16 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 16
 
+mkdir abp
+cd abp
+
+git clone https://github.com/carlosmn25/ABP-PyGITIC.git
+cd ABP-PyGITIC
+git checkout origin/carlos
+git switch carlos
+
+cd src/ec2/statistics-webpage 
+npm install
+
+node index.js
 EOF

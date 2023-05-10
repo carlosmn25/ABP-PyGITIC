c7fb9b350ba1206a3824e8f12027c7649a14f64c
Jesús Jover <jesus.jover@alu.uclm.es>
2023-05-10 12:55:37 +0200

    Solucionados bugs y eliminación de comentarios

--- src/lambdas/main.tf
+++ src/lambdas/main.tf
@@ -39,9 +39,9 @@ resource "aws_lambda_function_url" "url1" {
 
 // Lambda para ofrecer datos de los puntos de carga a los coches (Car API)
 resource "aws_lambda_function" "lambda_car_api" {
-  filename      = "${path.module}/python/hello-python.zip"
+  filename      = "${path.module}/python/code.zip"
   function_name = "Car-API-Lambda-Function"
-  role          = "arn:aws:iam::595531780921:role/LabRole" //TODO: Tomarlo de una variable o similar para que sea configurable
+  role          = "arn:aws:iam::${var.account_id}:role/LabRole"
   handler       = "car_api_code.lambda_handler"
   runtime       = "python3.8"
 }
@@ -59,55 +59,3 @@ resource "aws_lambda_function_url" "url_car_api" {
     max_age           = 86400
   }
 }
-
-/*resource "aws_api_gateway_rest_api" "api_gateway" {
-  name = "Data-API-Gateway"
-}
-
-resource "aws_api_gateway_method" "post_form" {
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  resource_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "api_gateway_integration_root_post" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_method.post_form.resource_id
-  http_method = aws_api_gateway_method.post_form.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.terraform_lambda_func.invoke_arn
-}
-
-resource "aws_api_gateway_method" "options_form" {
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  resource_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "api_gateway_integration_root_option" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_method.options_form.resource_id
-  http_method = aws_api_gateway_method.options_form.http_method
-
-  integration_http_method = "OPTIONS"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.terraform_lambda_func.invoke_arn
-}
-
-resource "aws_api_gateway_deployment" "api_gateway_deployment" {
-  depends_on  = [aws_api_gateway_integration.api_gateway_integration_root_post]
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  stage_name  = "stage-api"
-}
-
-output "lambda_function_invoke_arn" {
-  value = aws_lambda_function.terraform_lambda_func.invoke_arn
-}
-
-output "lambda_function_arn" {
-  value = aws_lambda_function.terraform_lambda_func.lambda_arn
-}*/

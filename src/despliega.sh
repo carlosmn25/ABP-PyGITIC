7f10b0bdd89d528ad05580c3188eab72fc2874be
ajtarraga <antoniojoaquin231999@gmail.com>
2023-05-15 19:48:18 +0200

    Actualización para mejorar el formato de la salida

--- src/despliega.sh
+++ src/despliega.sh
@@ -2,18 +2,44 @@
 
 # But export as a variable in the environment
 export TF_VAR_account_id=$(aws sts get-caller-identity | grep Account | cut -d '"' -f 4)
+
+echo
+echo "==============================="
+echo "        TERRAFORM INIT"
+echo "==============================="
+echo
+
 terraform init
+
+echo
+echo "==============================="
+echo "        TERRAFORM PLAN"
+echo "==============================="
+echo "See the plan in tf_plan.out"
+echo
+
 terraform plan -out tf_plan.out > /dev/null
+
+echo
+echo "==============================="
+echo "        TERRAFORM APPLY"
+echo "==============================="
+echo
+
 terraform apply -auto-approve
 
 # Take the url of the lambda called 'Car-API-Lambda-Function'
 url=$(aws lambda get-function-url-config --function-name Car-API-Lambda-Function | grep FunctionUrl | cut -d '"' -f 4)
-echo -e "\nThe url of the lambda car_api is: $url"
+echo
+echo "\nThe url of the lambda car_api is: $url"
 
 # Take the url of the lambda called 'Sensor-Lambda-Function'
 url=$(aws lambda get-function-url-config --function-name Sensor-Lambda-Function | grep FunctionUrl | cut -d '"' -f 4)
-echo -e "\nThe url of the lambda sensor is: $url"
+echo
+echo "\nThe url of the lambda sensor is: $url"
 
 # Take the url of the ec2 instance called 'statistics-webserver'
 url=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=statistics-webserver" | grep PublicIpAddress | cut -d '"' -f 4)
-echo -e "\nThe url of the statistics webserver is: http://$url:3000"
+echo
+echo "\nThe url of the statistics webserver is: http://$url:3000"
+echo
